2e1a8557f50529bd0f9d6c69dae8987638b472c5
Jiazhi Guo <jerrykuo7727@gmail.com>
2017-09-07 16:06:43 +0800

    Finish guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,3 +4,24 @@
 function get_file_count {
   echo $(ls -l | grep ^- | wc -l)
 }
+
+echo "Please guess how many files are in the current directory: "
+file_count=$(get_file_count)
+
+read guess
+while [[ $guess -ne $file_count ]]
+do
+  if [[ $guess -lt $file_count ]]
+  then
+    echo "Too low."
+  elif [[ $guess -gt $file_count ]]
+  then
+    echo "Too high."
+  else
+    break
+  fi
+
+  read guess
+done
+
+echo "Correct. Congratulations!"
